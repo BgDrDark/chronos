c4fd9a9c5bcf481caced83afac8f59ef4be2b05d
BgDrDark <151470543+BgDrDark@users.noreply.github.com>
2026-05-31 08:28:59 +0300

    remove hardcoded MAIL_PASSWORD from config.py

--- backend/config.py
+++ backend/config.py
@@ -135,7 +135,7 @@ class Settings(BaseSettings):
 
     # SMTP Settings
     MAIL_USERNAME: str | None = "thewall@abv.bg"
-    MAIL_PASSWORD: str | None = "POcboRFw55"
+    MAIL_PASSWORD: str | None = None
     MAIL_FROM: str = "thewall@abv.bg"
     MAIL_PORT: int = 465
     MAIL_SERVER: str = "smtp.abv.bg"
